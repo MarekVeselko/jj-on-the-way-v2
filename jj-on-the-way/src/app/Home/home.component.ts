4bc7b5f320626db134d0a915c13ecb46fbbbf779
Marek Veselko <veselko.marek@gmail.com>
2022-12-12 21:57:00 +0100

    Fonty

--- jj-on-the-way/src/app/Home/home.component.ts
+++ jj-on-the-way/src/app/Home/home.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef, HostListener  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef, HostListener, Renderer2, Inject, OnDestroy } from '@angular/core';
 import { Article } from '../shared/models/article.model';
 import { ArticlesService } from '../shared/services/articles.service';
 import { MapService } from '../shared/services/map.service';
@@ -12,26 +13,31 @@ import { MapService } from '../shared/services/map.service';
 export class HomeComponent implements OnInit {
   articles!: Article[];
   public screenWidth!: number;
-
-  pins!: {x: string, y: string}[];
+  script: any;
+  pins!: { x: string, y: string }[];
 
   constructor(private articleService: ArticlesService,
     private mapService: MapService,
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
     private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.screenWidth = window.innerWidth;
 
+    this.script = this.renderer2.createElement('script');
+    this.script.text = '(function(d, s, id) { var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript"));';
+    this.renderer2.appendChild(this.document.body, this.script);
     this.articleService.getArticles('PUBLISHED').subscribe(response => {
       if (response.length >= 5) {
-        this.articles = response.slice(0,5);
+        this.articles = response.slice(0, 5);
       } else {
         this.articles = response;
       }
       this.cdr.markForCheck();
     })
 
-    this.mapService.getMap().subscribe( response => {
+    this.mapService.getMap().subscribe(response => {
       this.pins = response[0].pins;
     })
   }
